b2a649842853fb2d0aefa5260ea5ba91e143be81
Kiran Balam <kiranbalam25@gmail.com>
2026-07-28 23:11:43 +0530

    Add files via upload

---
+++ Odd_Even_Checker.py
@@ -0,0 +1,8 @@
+#even or odd checker
+
+print("Welcome to odd or even checker")
+num = int(input("Enter the number you want to check :"))
+if(num % 2==0):
+    print("The number is even")
+else :
+    print("The number is odd")
