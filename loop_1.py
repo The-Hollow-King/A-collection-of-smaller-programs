11a82d3677b36736b6f59a3957ac7721d7efd34d
Kiran Balam <kiranbalam25@gmail.com>
2026-07-30 16:48:14 +0530

    Add files via upload

---
+++ loop_1.py
@@ -0,0 +1,10 @@
+#loop program
+start,end,step=input("enter start,end,step values:").split()
+#right aligned printing
+for n in range (int(start),int(end),int(step)):
+    print(f"{n:>5}{n**2:>7}{n**3:>8}")
+print()
+
+#left aligned printing
+for n in range (int(start), int(end), int(step)):
+    print("{0:<5}{1:<7}{2:<8}".format(n,n**2,n**3))
