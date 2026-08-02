5dd8ce363746a3cdd198260690b06e7c0b8a9d95
Kiran Balam <kiranbalam25@gmail.com>
2026-08-02 16:43:46 +0530

    Add files via upload

---
+++ Celsius to fahrenheit conversion.py
@@ -0,0 +1,10 @@
+#program to convert Celsius to Fahrenheit
+
+#intializing a variable to accept temperature in celsius and store its value
+cel = int(input("Enter the temperature in celsius :"))
+
+#creating a variable to convert celsius into fahrenheit
+fah = (1.8*cel) +32
+
+#printing the output
+print("The given temperature in fahrenheit is :", +fah)
