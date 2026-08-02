4fb1622894ad23749fe630446c767daa55c46623
Kiran Balam <kiranbalam25@gmail.com>
2026-08-02 16:43:15 +0530

    Add files via upload

---
+++ calculator program.py
@@ -0,0 +1,18 @@
+'''interactive program to accept two number and display the addition, subtraction, multiplication
+and division of those numbers'''
+
+#intializing the two variables
+num1 = float(input("Enter the first number :"))
+num2 = float(input("Enter the second number :"))
+
+#intializing other variables to perform operation on num1 and num2
+add = num1 + num2
+subt = num1 - num2
+multi = num1 * num2
+divi = num1/num2
+
+#printing the outputs
+print("The sum of addition of the two numbers is :", +add)
+print("The sum of subtraction of the two numbers is :", +subt)
+print("The sum of multiplication of the two numbers is :", +multi)
+print("The sum of division of the two numbers is :", +divi)
